273509a827066486cc744ea126cb99ab49c00164
JeanBaptisteR <robert.jeanbaptiste@laposte.net>
2017-01-17 10:49:35 +0100

    Implémentation de la gestion du last 10 search à l'aide de Redis dans la méthode

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
@@ -1,10 +1,23 @@
 package com.serli.oracle.of.bacon.repository;
 
+import redis.clients.jedis.Jedis;
+
 import java.util.List;
 
 public class RedisRepository {
+
+    Jedis jedis;
+
+    public RedisRepository () {
+        jedis = new Jedis("localhost", 6379);
+    }
+
+    public void addSearch(String search) {
+        jedis.lpush("searches", search);
+        jedis.rpop("searches");
+    }
+
     public List<String> getLastTenSearches() {
-        // TODO implement last 10 searchs
-        return null;
+        return jedis.lrange("searches", 0, 9);
     }
 }
