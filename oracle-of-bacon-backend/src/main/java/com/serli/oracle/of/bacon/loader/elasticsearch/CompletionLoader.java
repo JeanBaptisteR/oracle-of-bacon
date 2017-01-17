971456badd00177078a3912917a0a7525b7abad4
Mathieu THEBAUD <math.thebaud@gmail.com>
2017-01-17 14:17:09 +0100

    -Completion loader

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -1,35 +1,49 @@
 package com.serli.oracle.of.bacon.loader.elasticsearch;
 
-import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
-import io.searchbox.client.JestClient;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Index;
+
 public class CompletionLoader {
-    private static AtomicInteger count = new AtomicInteger(0);
+	private static final String INDEX_NAME = "actors";
+	private static final String TYPE_NAME = "actor";
 
-    public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.err.println("Expecting 1 arguments, actual : " + args.length);
-            System.err.println("Usage : completion-loader <actors file path>");
-            System.exit(-1);
-        }
+	private static AtomicInteger count = new AtomicInteger(0);
 
-        String inputFilePath = args[0];
-        JestClient client = ElasticSearchRepository.createClient();
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Expecting 1 arguments, actual : " + args.length);
+			System.err.println("Usage : completion-loader <actors file path>");
+			System.exit(-1);
+		}
 
-        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
-            bufferedReader.lines()
-                    .forEach(line -> {
-                        //TODO ElasticSearch insert
-                        System.out.println(line);
-                    });
-        }
+		String inputFilePath = args[0];
+		JestClient client = ElasticSearchRepository.createClient();
 
-        System.out.println("Inserted total of " + count.get() + " actors");
-    }
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
+			Bulk.Builder builder = new Bulk.Builder();
+			bufferedReader.lines().skip(1)// Skip header
+			/*.limit(100000)*/.forEach(actorName -> {
+						builder.addAction(new Index.Builder(//
+								new StringBuilder("{ \"name\": ")//
+										.append(actorName)//Actor already have "
+										.append(" }").toString()//
+						).index(INDEX_NAME).type(TYPE_NAME).build());
+						count.incrementAndGet();
+					});// Add to bulk action
+			// Execute bulk
+			Bulk bulk = builder.build();
+			System.out.println("Bulk created");
+			client.execute(bulk);
+		}
+		System.out.println("Inserted total of " + count.get() + " actors");
+	}
 }
